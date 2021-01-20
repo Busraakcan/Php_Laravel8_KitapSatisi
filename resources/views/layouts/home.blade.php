de42e5e4c5c075671d75961086fc3c81b72099bf
Busraakcan <busra4216akcan@gmail.com>
2021-01-20 16:41:12 +0300

    Fourth Commit
    Hazır Html Web Template Uyarlaması

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -1,39 +1,47 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="zxx">
+
 <head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Ogani Template">
+    <meta name="keywords" content="Ogani, unica, creative, html">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <meta name="description" content="@yield('description')">
+    <meta name="keywords" content="@yield('keywords')">
+    <meta name="author" content="Busra Akcan">
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;600;900&display=swap" rel="stylesheet">
+
+    <!-- Css Styles -->
+    <link rel="stylesheet" href="{{asset('assets')}}/home//css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="{{asset('assets')}}/home/css/font-awesome.min.css" type="text/css">
+    <link rel="stylesheet" href="{{asset('assets')}}/home/css/elegant-icons.css" type="text/css">
+    <link rel="stylesheet" href="{{asset('assets')}}/home/css/nice-select.css" type="text/css">
+    <link rel="stylesheet" href="{{asset('assets')}}/home/css/jquery-ui.min.css" type="text/css">
+    <link rel="stylesheet" href="{{asset('assets')}}/home/css/owl.carousel.min.css" type="text/css">
+    <link rel="stylesheet" href="{{asset('assets')}}/home/css/slicknav.min.css" type="text/css">
+    <link rel="stylesheet" href="{{asset('assets')}}/home/css/style.css" type="text/css">
     @yield('css')
-    @yield('js')
-
+    @yield('headerjs')
 </head>
+
 <body>
-
-<div class="jumbotron text-center">
-    <h1>Header</h1>
-    <p>Resize this responsive page to see the effect!</p>
-    <a href="/">Home</a>
-    <a href="/aboutus">About Us</a>
-    @yield('header')
-</div>
-
-<div class="container">
-    <div class="row">
-        @yield('sidebar')
-        @yield('content')
+@include('home._header')
+<section class="hero">
+    <div class="container">
+        <div class="row">
+            @include('home._category')
+            @include('home._menu')
+        </div>
     </div>
-</div>
-
-<div class="jumbotron text-center">
-    <h1>Footor</h1>
-    <p>sayfa altı</p>
-    @yield('footor')
-</div>
+</section>
+@include('home._categorical')
+@section('content')
+@show
+@include('home._footer')
+@yield('footerjs') 
 
 
 
